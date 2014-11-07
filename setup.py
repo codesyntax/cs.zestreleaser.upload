2166a74f8b77fd69c23fd52e8a87f371d5b4cf24
Mikel Larreategi <mlarreategi@codesyntax.com>
2014-11-07 12:41:13 +0100

    docs

--- setup.py
+++ setup.py
@@ -3,34 +3,52 @@ import os
 
 version = '1.0'
 
-setup(name='cs.zestreleaser.upload',
-      version=version,
-      description="zest.releaser plugin to enter from the command-line the destination of the upload",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
-      classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
+setup(
+    name='cs.zestreleaser.upload',
+    version=version,
+    description="zest.releaser plugin to enter from the command-line the destination of the upload",
+    long_description=open("README.txt").read() + "\n" +
+                     open(os.path.join("docs", "HISTORY.txt")).read(),
+    # Get more strings from
+    # http://pypi.python.org/pypi?:action=list_classifiers
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'License :: OSI Approved :: Zope Public License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2 :: Only',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Archiving :: Packaging',
+        'Topic :: System :: Installation/Setup',
+        'Topic :: Utilities',
         ],
-      keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['cs', 'cs.zestreleaser'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
+    keywords='',
+    author='Mikel Larreategi',
+    author_email='mlarreategi@codesyntax.com',
+    url='https://github.com/codesyntax/cs.zestreleaser.upload',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['cs', 'cs.zestreleaser'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        # -*- Extra requirements: -*-
+        'zest.releaser',
+    ],
+    entry_points={
+        'zest.releaser.releaser.after':
+            ['upload=gocept.zestreleaser.customupload.upload:upload',
+            'upload_doc=gocept.zestreleaser.customupload.doc:upload']},
       )
